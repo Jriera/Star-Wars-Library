877182ffd3e0b783f6518969b2922c6289d1a840
Jriera <jordiriera81@gmail.com>
2021-11-15 16:22:32 +0100

    Crea directiva per gestio d'errors de carrega de imatge
    La API proporcionada per al projecte no conte totes les imatges necessaries per a cada un dels elements imatge. Per aquest motiu es crea una directiva per a plicar a les imatger per detectar quan es produeix un error de carrega d'imatge i poder efectuar una nova src a partir de la crida a una api alternativa

---
+++ src/app/image-error-handler.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImageErrorHandlerDirective } from './image-error-handler.directive';
+
+describe('ImageErrorHandlerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImageErrorHandlerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
