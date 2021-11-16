801c00b8e6a85f06ec1ae8378f4d692c0a366f0e
Jriera <jordiriera81@gmail.com>
2021-11-16 16:08:36 +0100

    Creacio d'un servei d'autentificacio, encara no implementat

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  createUser(email: string, password: string, name: string) {
+    
+  }
+
+}
