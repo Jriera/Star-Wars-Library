1e2369cb1a350574321e99f53e8b4336854e824b
Jriera <jordiriera81@gmail.com>
2021-11-16 17:48:08 +0100

    Implement user creation and authentication methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,25 @@ export class AuthService {
 
   constructor() { }
 
-  createUser(email: string, password: string, name: string) {
-    
+  createUser(email: string, password: string, name: string,lname:string) {
+    // Here you can create a new user
+    const user:User = {
+      name: name,
+      lname:lname,
+      password: password,
+      email: email
+    }
+    localStorage.setItem('user', JSON.stringify(user));
   }
+
+  authenticate(email: string, password: string) {
+    
+    const user:User=JSON.parse(localStorage.getItem('user')||'{}');
+    user.email===email && user.password===password ? true : false;
+    
+
+  }
+
+
 
 }
