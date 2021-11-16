f3726520f9fca7483ae9a4367e97947c97f462ef
Jriera <jordiriera81@gmail.com>
2021-11-16 16:06:31 +0100

    Afegeix login i signup
    Es crea la logica per obrir els components de login i signup desde els enllaços creats al component navegacio

--- src/app/components/nav-component/nav-component.component.ts
+++ src/app/components/nav-component/nav-component.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { LoginModalComponent } from '../login-modal/login-modal.component';
+import { SignupComponent } from 'src/app/signup/signup.component';
 @Component({
   selector: 'app-nav-component',
   templateUrl: './nav-component.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dialog:MatDialog) { }
+    
   ngOnInit(): void {
   }
+  
+  openLoginModal(){
+    this.dialog.open(LoginModalComponent,{
+      backdropClass:'backdrop'
+    });
+  }
 
+  openSingupModal(){
+    this.dialog.open(SignupComponent,{
+      backdropClass:'backdrop'
+    });
+  }
 }
+
