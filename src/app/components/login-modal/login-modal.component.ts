162a00f00d6b11610c7a011c25a0c04521dc472c
Jriera <jordiriera81@gmail.com>
2021-11-16 19:17:10 +0100

    Crea Form controls i implemetacio del servei auth per autentiar usuaris

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-modal',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginModalComponent implements OnInit {
 
-  constructor() { }
+  userLogin=new FormGroup({
+    emailForm:new FormControl(''),
+    passwordForm:new FormControl('')
+  })
+  constructor(private auth:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.auth.authenticate(this.userLogin.value.emailForm,this.userLogin.value.passwordForm)
   }
 
 }
