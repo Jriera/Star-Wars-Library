59f591a00d097b8280344a6e6676c2fe453e70c0
Jriera <jordiriera81@gmail.com>
2021-11-16 18:22:45 +0100

    inclou authguard com a proteccio de rutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { StarshipsComponent } from './components/starships/starships.component';
 import { StarshipDetailsComponent } from './components/starship-details/starship-details.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'starships',component: StarshipsComponent},
-  { path: 'starship/:id',component: StarshipDetailsComponent}
+  { path: 'starships',component: StarshipsComponent, canActivate: [AuthGuard] },
+  { path: 'starship/:id',component: StarshipDetailsComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
