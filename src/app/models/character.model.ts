9a0c28c3f648697d59a809b7331a854955beb782
Jriera <jordiriera81@gmail.com>
2021-11-17 22:08:45 +0100

    Actualitza model personatge

--- src/app/models/character.model.ts
+++ src/app/models/character.model.ts
@@ -2,4 +2,18 @@ export interface Character {
     name: string;
     image: string;
     id: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color:string;
+
+    eye_color:string;
+    birth_year:string;
+    gender:string;
+    homeworld: string;
+    films:string[];
+    species:string[],
+    vehicles: string[]
+    starships: string[];
+    url:string
 }
