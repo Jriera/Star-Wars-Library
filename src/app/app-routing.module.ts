e97d57c4869fa8d40e9c114db30f1033a77dd6ef
Jriera <jordiriera81@gmail.com>
2021-11-17 22:13:59 +0100

    Crea routing per al component actors
    El component tambe esta protegit per authguard, de manera que l'access es no mes diponible per a usuaris loggats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { StarshipsComponent } from './components/starships/starships.component';
 import { StarshipDetailsComponent } from './components/starship-details/starship-details.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ActorsComponent } from './components/actors/actors.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'starships',component: StarshipsComponent, canActivate: [AuthGuard] },
   { path: 'starship/:id',component: StarshipDetailsComponent, canActivate: [AuthGuard]},
+  { path: 'actors',component: ActorsComponent, canActivate: [AuthGuard]},
   {path:'login',component:LoginComponent}
 ];
 
