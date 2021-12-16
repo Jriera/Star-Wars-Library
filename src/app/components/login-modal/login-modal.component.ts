810d434844a5fbec7e58eec820bd785d317c71eb
Jriera <jordiriera81@gmail.com>
2021-12-16 22:57:46 +0100

    fix:Correction to allow signup from signin modal

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/services/auth.service';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-login-modal',
@@ -14,15 +16,21 @@ export class LoginModalComponent implements OnInit {
     emailForm:new FormControl(''),
     passwordForm:new FormControl('')
   })
-  constructor(private auth:AuthService, private router:Router) { }
+  constructor(private auth:AuthService, private router:Router,private dialog:MatDialog) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.auth.authenticate(this.userLogin.value.emailForm,this.userLogin.value.passwordForm)
-    this.router.navigate(['/starships'])
+    this.router.navigate(['starships'])
 
+  }
+
+  openSingupModal(){
+    this.dialog.open(SignupComponent,{
+      backdropClass:'backdrop'
+    });
   }
 
 }
